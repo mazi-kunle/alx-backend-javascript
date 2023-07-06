aabc3e871f1da9d607cd35c13f653720a0ce89ab
olakunle <olakunlemichael644@gmail.com>
2023-07-06 08:07:17 -0700

    100

--- 0x01-ES6_promise/100-await.js
+++ 0x01-ES6_promise/100-await.js
@@ -1,10 +1,15 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default async function asyncUploadUser() {
-  const photo = await uploadPhoto();
-  const user = await createUser();
-  return {
-    photo,
-    user,
-  };
+  let res = {};
+
+  try {
+    const photo = await uploadPhoto();
+    const user = await createUser();
+    res = { photo, user };
+  } catch (err) {
+    res = { photo: null, user: null };
+  }
+
+  return res;
 }
