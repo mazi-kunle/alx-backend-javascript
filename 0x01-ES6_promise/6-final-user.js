95010b9a69d92a4166e679dfc952ba5d31541d49
mazi-kunle <102045752+mazi-kunle@users.noreply.github.com>
2023-07-06 13:30:48 +0100

    Update 6-final-user.js

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,19 +1,13 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-export default function handleProfileSignup(firstName, lastName, fileName) {
-  const promises = [
-    signUpUser(firstName, lastName),
-    uploadPhoto(fileName),
-  ];
-  const promise = Promise.allSettled(promises);
-  promise.then((res) => {
-    const newArr = res.map((o) => ({
-      status: o.status,
-      value: o.status === 'fulfilled' ? o.value : String(o.reason),
-    }));
-    console.log(newArr);
-  });
-
-  return promise;
+export default async function handleProfileSignup(firstName, lastName, fileName) {
+  return Promise
+    .allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
+    .then((res) => (
+      res.map((o) => ({
+        status: o.status,
+        value: o.status === 'fulfilled' ? o.value : String(o.reason),
+      }))
+    ));
 }
