47c0e2c213f69f034db19880145a80ac87c4670b
mazi-kunle <olakunlemichael644@gmail.com>
2023-07-11 14:42:32 +0100

    task 5

---
+++ 0x02-ES6_classes/5-building.js
@@ -0,0 +1,17 @@
+/* eslint class-methods-use-this: ["error", { "exceptMethods": ["evacuationWarningMessage"] }] */
+
+export default class Building {
+  constructor(sqft) {
+    this._sqft = sqft;
+    if (this.constructor !== Building
+        && typeof this.evacuationWarningMessage !== 'function') {
+      throw new Error(
+        'Class extending Building must override evacuationWarningMessage',
+      );
+    }
+  }
+
+  get sqft() {
+    return this._sqft;
+  }
+}
