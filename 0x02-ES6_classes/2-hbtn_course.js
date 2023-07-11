9cecb733845834cad86bbc492cdbd4bcb208b28f
mazi-kunle <olakunlemichael644@gmail.com>
2023-07-11 04:57:31 +0100

    2

---
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -0,0 +1,41 @@
+export default class HolbertonCourse {
+  constructor(name, length, students) {
+    this.name = name;
+    this.length = length;
+    this.students = students;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(newName) {
+    if (typeof (newName) !== 'string') {
+      throw new Error('TypeError: Name must be a string');
+    }
+    this._name = newName;
+  }
+
+  get length() {
+    return this._length;
+  }
+
+  set length(newLength) {
+    if (typeof (newLength) !== 'number') {
+      throw new Error('TypeError: Length must be a number');
+    }
+    this._length = newLength;
+  }
+
+  get students() {
+    return this._students;
+  }
+
+  set students(newStudents) {
+    if (Array.isArray(newStudents)) {
+      this._students = newStudents;
+    } else {
+      throw new Error('TypeError: Students must be an array');
+    }
+  }
+}
