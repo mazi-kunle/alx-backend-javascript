60cc6dba912cadead39f30786d4bd800b0d10d31
mazi-kunle <olakunlemichael644@gmail.com>
2023-07-14 21:16:17 +0100

    task 8

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,17 +1,23 @@
 export default function cleanSet(cleanSetset, startString) {
   let finalStr = '';
   let flag = 0;
+  if (!cleanSetset || !startString || !(cleanSetset instanceof Set) || typeof startString !== 'string') {
+    return '';
+  }
+
   cleanSetset.forEach((element) => {
-    const start = element.indexOf(startString);
-    let subStr;
-    if (start === 0 && startString !== '') {
-      if (flag === 1) {
-        finalStr = finalStr.concat('-');
+    if (element !== undefined) {
+      const start = element.indexOf(startString);
+      let subStr;
+      if (start === 0 && startString !== '') {
+        if (flag === 1) {
+          finalStr = finalStr.concat('-');
+        }
+        const endIndex = startString.length;
+        subStr = element.slice(endIndex);
+        finalStr = finalStr.concat(subStr);
+        flag = 1;
       }
-      const endIndex = startString.length;
-      subStr = element.slice(endIndex);
-      finalStr = finalStr.concat(subStr);
-      flag = 1;
     }
   });
   return finalStr;
