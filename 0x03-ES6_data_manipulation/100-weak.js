e493f4d44ac3c2df0e28b179af6924a396b7340b
mazi-kunle <olakunlemichael644@gmail.com>
2023-07-17 20:53:05 +0100

    task 100

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -4,9 +4,8 @@ export function queryAPI(val) {
   if (!(weakMap.has(val))) {
     weakMap.set(val, 0);
   }
-  if (weakMap.get(val) < 5) {
-    weakMap.set(val, weakMap.get(val) + 1);
-  } else {
+  weakMap.set(val, weakMap.get(val) + 1);
+  if (weakMap.get(val) >= 5) {
     throw new Error('Endpoint load is high');
   }
   return weakMap.get(val);
