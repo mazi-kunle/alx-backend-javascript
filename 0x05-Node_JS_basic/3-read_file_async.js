028403e64ba9172c4aeb474fc245c95f36121565
mazi-kunle <olakunlemichael644@gmail.com>
2023-08-22 19:41:59 +0100

    task 3

---
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -0,0 +1,34 @@
+const fs = require('fs');
+
+function countStudents(path) {
+  return new Promise((resolve, reject) => {
+    const db = {};
+    fs.readFile(path, 'utf-8', (err, data) => {
+      if (err) {
+        reject(new Error('cannot load the database'));
+        return;
+      }
+
+      const contents = data.toString().split('\n');
+
+      for (let i = 1; i < contents.length; i += 1) {
+        const field = contents[i].split(',');
+
+        if (!(Object.hasOwn(db, field[3])) && field[3] !== undefined) {
+          const temp = [field[0]];
+          db[field[3]] = temp;
+        } else if (field[3] !== undefined) {
+          db[field[3]].push(field[0]);
+        }
+      }
+      console.log(`Number of students: ${contents.length - 1}`);
+      Object.keys(db).forEach((data) => {
+        console.log(`Number of students in ${data}: ${db[data].length}. List: ${db[data].join(', ')}`);
+      });
+
+      resolve();
+    });
+  });
+}
+
+module.exports = countStudents;
