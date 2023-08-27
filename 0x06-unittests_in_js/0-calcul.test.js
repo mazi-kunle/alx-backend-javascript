c0eca30932dee92eba462d2ee5097fe3212784ac
mazi-kunle <olakunlemichael644@gmail.com>
2023-08-27 23:34:35 +0100

    fix

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -1,17 +1,17 @@
 const assert = require('assert');
-const calculateNUmber = require('./0-calcul');
+const calculateNumber = require('./0-calcul');
 
 describe('Tests for calculateNumber', () => {
   it('Returns 6', () => {
-    assert.equal(calculateNUmber(2.5, 3), 6);
+    assert.equal(calculateNumber(2.5, 3), 6);
   });
   it('Returns 5', () => {
-    assert.equal(calculateNUmber(1, 3.7), 5);
+    assert.equal(calculateNumber(1, 3.7), 5);
   });
   it('Returns 6', () => {
-    assert.equal(calculateNUmber(1.5, 3.7), 6);
+    assert.equal(calculateNumber(1.5, 3.7), 6);
   });
   it('Returns 4', () => {
-    assert.equal(calculateNUmber(1, 3), 4);
+    assert.equal(calculateNumber(1, 3), 4);
   });
 });
