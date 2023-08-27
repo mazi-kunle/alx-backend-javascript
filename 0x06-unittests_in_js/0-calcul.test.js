e76b743cf5d21e2f11648c781f277e0ba503e399
mazi-kunle <olakunlemichael644@gmail.com>
2023-08-27 16:54:29 +0100

    edited test file

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -4,18 +4,18 @@ const calculateNUmber = require('./0-calcul');
 describe('calculateNumber', () => {
   it('test when first num is rounded', () => {
     const result = calculateNUmber(2.5, 3);
-    assert(result === 6);
+    assert.strictEqual(result, 6);
   });
   it('test when second num is rounded', () => {
     const result = calculateNUmber(1, 3.7);
-    assert(result === 5);
+    assert.strictEqual(result, 5);
   });
   it('test when both numbers are rounded', () => {
     const result = calculateNUmber(1.5, 3.7);
-    assert(result === 6);
+    assert.strictEqual(result, 6);
   });
   it('test when both numbers are not rounded', () => {
     const result = calculateNUmber(1, 3);
-    assert(result === 4);
+    assert.strictEqual(result, 4);
   });
 });
