124eb4b1d915a1c4f274ad1ec62f7eaf08f758ca
mazi-kunle <olakunlemichael644@gmail.com>
2023-08-27 17:27:32 +0100

    task 0

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -4,18 +4,18 @@ const calculateNUmber = require('./0-calcul');
 describe('Tests for calculateNumber', () => {
   it('Returns 6', () => {
     const result = calculateNUmber(2.5, 3);
-    assert.strictEqual(result, 6);
+    assert.equal(result, 6);
   });
   it('Returns 5', () => {
     const result = calculateNUmber(1, 3.7);
-    assert.strictEqual(result, 5);
+    assert.equal(result, 5);
   });
   it('Returns 6', () => {
     const result = calculateNUmber(1.5, 3.7);
-    assert.strictEqual(result, 6);
+    assert.equal(result, 6);
   });
   it('Returns 4', () => {
     const result = calculateNUmber(1, 3);
-    assert.strictEqual(result, 4);
+    assert.equal(result, 4);
   });
 });
