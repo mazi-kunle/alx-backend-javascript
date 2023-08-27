d59f3ecb07034903aec4e782b139640475495851
mazi-kunle <olakunlemichael644@gmail.com>
2023-08-27 17:03:48 +0100

    task 0

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -1,21 +1,29 @@
 const assert = require('assert');
 const calculateNUmber = require('./0-calcul');
 
-describe('calculateNumber', () => {
-  it('test when first num is rounded', () => {
-    const result = calculateNUmber(2.5, 3);
-    assert.strictEqual(result, 6);
+describe('Tests for calculateNumber', () => {
+  describe('test when first num is rounded', () => {
+    it('Returns 6', () => {
+      const result = calculateNUmber(2.5, 3);
+      assert.strictEqual(result, 6);
+    });
   });
-  it('test when second num is rounded', () => {
-    const result = calculateNUmber(1, 3.7);
-    assert.strictEqual(result, 5);
+  describe('test when second num is rounded', () => {
+    it('Returns 5', () => {
+      const result = calculateNUmber(1, 3.7);
+      assert.strictEqual(result, 5);
+    });
   });
-  it('test when both numbers are rounded', () => {
-    const result = calculateNUmber(1.5, 3.7);
-    assert.strictEqual(result, 6);
+  describe('test when both numbers are rounded', () => {
+    it('Returns 6', () => {
+      const result = calculateNUmber(1.5, 3.7);
+      assert.strictEqual(result, 6);
+    });
   });
-  it('test when both numbers are not rounded', () => {
-    const result = calculateNUmber(1, 3);
-    assert.strictEqual(result, 4);
+  describe('test when both numbers are not rounded', () => {
+    it('Returns 4', () => {
+      const result = calculateNUmber(1, 3);
+      assert.strictEqual(result, 4);
+    });
   });
 });
