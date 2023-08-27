3e73b83c1043848adce98af53992c0a17248476f
mazi-kunle <olakunlemichael644@gmail.com>
2023-08-27 18:17:31 +0100

    task 2

--- 0x06-unittests_in_js/2-calcul_chai.test.js
+++ 0x06-unittests_in_js/2-calcul_chai.test.js
@@ -2,16 +2,24 @@ const { expect } = require('chai');
 const calculateNUmber = require('./1-calcul');
 
 describe('Tests for calculateNumber', () => {
-  it('Returns 6', () => {
-    expect(calculateNUmber('SUM', 2.5, 3)).to.equal(6);
+  describe('test for SUM', () => {
+    it('Returns 6', () => {
+      expect(calculateNUmber('SUM', 2.5, 3)).to.equal(6);
+    });
   });
-  it('Returns -4', () => {
-    expect(calculateNUmber('SUBTRACT', 1.4, 4.5)).to.equal(-4);
+  describe('test for SUBTRACT', () => {
+    it('Returns -4', () => {
+      expect(calculateNUmber('SUBTRACT', 1.4, 4.5)).to.equal(-4);
+    });
   });
-  it('Returns 0.2', () => {
-    expect(calculateNUmber('DIVIDE', 1.4, 4.5)).to.equal(0.2);
+  describe('test for DIVIDE', () => {
+    it('Returns 0.2', () => {
+      expect(calculateNUmber('DIVIDE', 1.4, 4.5)).to.equal(0.2);
+    });
   });
-  it('Returns Error', () => {
-    expect(calculateNUmber('DIVIDE', 1.4, 0)).to.equal('Error');
+  describe('test for DIVIDE (returns error)', () => {
+    it('Returns Error', () => {
+      expect(calculateNUmber('DIVIDE', 1.4, 0)).to.equal('Error');
+    });
   });
 });
