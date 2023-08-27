0d08bb4ae7eab1c617f5f67dfab86e2afd86025e
mazi-kunle <olakunlemichael644@gmail.com>
2023-08-27 23:43:19 +0100

    test

--- 0x06-unittests_in_js/2-calcul_chai.test.js
+++ 0x06-unittests_in_js/2-calcul_chai.test.js
@@ -2,22 +2,32 @@ const { expect } = require('chai');
 const calculateNUmber = require('./1-calcul');
 
 describe('Tests for calculateNumber', () => {
-  describe('test for SUM', () => {
+  describe('SUM first round', () => {
     it('Returns 6', () => {
       expect(calculateNUmber('SUM', 2.5, 3)).to.equal(6);
     });
   });
-  describe('test for SUBTRACT', () => {
+  describe('SUM second round', () => {
+    it('Returns 5', () => {
+      expect(calculateNUmber('SUM', 2, 3.4)).to.equal(5);
+    });
+  });
+  describe('SUM no round', () => {
+    it('Returns 2', () => {
+      expect(calculateNUmber('SUM', 1, 1)).to.equal(2);
+    });
+  });
+  describe('SUBTRACT both round', () => {
     it('Returns -4', () => {
       expect(calculateNUmber('SUBTRACT', 1.4, 4.5)).to.equal(-4);
     });
   });
-  describe('test for DIVIDE', () => {
+  describe('DIVIDE both round', () => {
     it('Returns 0.2', () => {
       expect(calculateNUmber('DIVIDE', 1.4, 4.5)).to.equal(0.2);
     });
   });
-  describe('test for DIVIDE (returns error)', () => {
+  describe('DIVIDE fist round, second zero -> (returns error)', () => {
     it('Returns Error', () => {
       expect(calculateNUmber('DIVIDE', 1.4, 0)).to.equal('Error');
     });
