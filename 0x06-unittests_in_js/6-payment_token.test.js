a9135ec2acd621a39b383790a110a92948d0602a
mazi-kunle <olakunlemichael644@gmail.com>
2023-08-28 16:34:13 +0100

    task 6

--- 0x06-unittests_in_js/6-payment_token.test.js
+++ 0x06-unittests_in_js/6-payment_token.test.js
@@ -5,8 +5,11 @@ describe('test for getPaymentTokenFromApi', () => {
   it('async test 1', (done) => {
     getPaymentTokenFromAPI(true)
       .then((data) => {
-        expect(data).to.equal({ data: 'Successful response from the API' });
+        expect(data).to.own.include({ data: 'Successful response from the API' });
+        done();
+      })
+      .catch((err) => {
+        done(err);
       });
-    done();
   });
 });
