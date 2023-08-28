b8b1369b11b6c0d9353156874a0c28e0a3f157e9
mazi-kunle <olakunlemichael644@gmail.com>
2023-08-28 17:23:47 +0100

    task 8

--- 0x06-unittests_in_js/8-api/api.test.js
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -4,15 +4,17 @@ const request = require('request');
 describe('Index page', () => {
   const url = 'http://localhost:7865';
 
-  it('test correct status', () => {
+  it('test correct status', (done) => {
     request(url, (err, res, body) => {
       expect(res.statusCode).to.equal(200);
+      done();
     });
   });
 
-  it('test corerct result', () => {
+  it('test corerct result', (done) => {
     request(url, (err, res, body) => {
       expect(body).to.equal('Welcome to the payment system');
+      done();
     });
   });
 });
